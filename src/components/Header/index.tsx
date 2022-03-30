ebb14ca76c557727b729a0bec3e447b9bb4495c7
Alex Junior Kuss <alex.kuss@havan.com.br>
2022-03-30 17:42:27 -0300

    quase la

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link'
+import styles from './header.module.scss'
+
+export function Header(): JSX.Element {
+
+  return (
+    <header className={styles.headerContainer}>
+      <Link href="/">
+        <a>
+          <img src="/images/Logo.svg" alt="spacetraveling" />
+        </a>
+      </Link>
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client'
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 
+import { FiCalendar, FiUser } from 'react-icons/fi'
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { Header } from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -21,16 +29,123 @@ interface PostPagination {
 }
 
 interface HomeProps {
+  preview: boolean;
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ preview, postsPagination }: HomeProps): JSX.Element {
+  const [posts, setPosts] = useState<PostPagination>({
+    ...postsPagination,
+    results: postsPagination.results.map(post => ({
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    })),
+  });
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  async function loadMorePosts(): Promise<void> {
+    const response = await fetch(`${posts.next_page}`).then(data =>
+      data.json()
+    );
 
-//   // TODO
-// };
+    const postsResponseResults = response.results.map(post => ({
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    }));
+
+    const newPosts = {
+      ...posts,
+      next_page: response.next_page,
+      results: [...posts.results, ...postsResponseResults],
+    };
+
+    setPosts(newPosts);
+  }
+
+
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTraveling</title>
+      </Head>
+      <Header />
+      <main className={styles.container}>
+        <section>
+          {posts.results.map(post => (
+            <article className={styles.ContentPost} key={post.uid}>
+              <Link href={`/post/${post.uid}`}>
+                <a>
+                  <strong>{post.data.title}</strong>
+                  <p>{post.data.subtitle}</p>
+                  <div>
+                    <time>
+                      <FiCalendar />
+                      {post.first_publication_date}
+                    </time>
+                    <span>
+                      <FiUser />
+                      {post.data.author}
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            </article>
+          ))}
+          {posts.next_page && (
+            <button className={styles.LoadPosts} type='button' onClick={loadMorePosts}>
+              Carregar mais posts
+            </button>
+          )}
+          {preview && (
+            <aside>
+              <Link href="/api/exit-preview">
+                <a>Sair do modo Preview</a>
+              </Link>
+            </aside>
+          )}
+        </section>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  preview = false,
+  previewData,
+}) => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ],
+    {
+      pageSize: 2,
+      ref: previewData?.ref ?? null,
+    }
+  );
+
+  console.log(postsResponse)
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results,
+  };
+  // TODO
+  return {
+    props: {
+      postsPagination,
+      preview,
+    },
+    revalidate: 60 * 10, // 10 min
+  }
+};
